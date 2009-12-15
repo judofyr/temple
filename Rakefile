8dc7ae5964fb3cf91ba50da4c5cd685eda3af323
grosser <grosser.michael@gmail.com>
2009-12-15 14:37:58 +0800

    add Rakefile and make rake run tests

---
+++ Rakefile
@@ -0,0 +1,3 @@
+task :default => :spec
+require 'spec/rake/spectask'
+Spec::Rake::SpecTask.new {|t| t.spec_opts = ['--color']}
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
-$:.unshift File.dirname(__FILE__) + '/../lib'
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
 
+require 'rubygems'
 require 'temple'
 require 'bacon'
 
