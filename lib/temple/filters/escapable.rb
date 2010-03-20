d7b66570e83ffb9646d4740a20ded8fab929ac17
Magnus Holm <judofyr@gmail.com>
2010-03-20 18:21:08 +0100

    Escapable: Change syntax

--- lib/temple/filters/escapable.rb
+++ lib/temple/filters/escapable.rb
@@ -1,17 +1,24 @@
+require 'cgi'
+
 module Temple
   module Filters
     class Escapable
       def initialize(options = {})
-        @escaper = options[:escaper] || 'CGI.escapeHTML(%s.to_s)'
+        @escaper = options[:escaper] || 'CGI.escapeHTML((%s).to_s)'
       end
       
       def compile(exp)
         return exp if !exp.is_a?(Enumerable) || exp.is_a?(String)
         
-        if exp[0] == :static && is_escape?(exp[1])
-          [:static, eval(@escaper % exp[1][1].inspect)]
-        elsif is_escape?(exp)
-          @escaper % exp[1]
+        if is_escape?(exp)
+          case exp[1][0]
+          when :static
+            [:static, eval(@escaper % exp[1][1].inspect)]
+          when :dynamic
+            [:dynamic, @escaper % exp[1][1]]
+          else
+            raise "Escapable can only handle :static and :dynamic for the moment."
+          end
         else
           exp.map { |e| compile(e) }
         end
