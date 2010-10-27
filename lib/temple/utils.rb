937bdfdf1ede04324764ed6cb0c556d6abdd56f7
minad <mail@daniel-mendler.de>
2010-10-27 23:17:38 +0200

    remove unused method

--- lib/temple/utils.rb
+++ lib/temple/utils.rb
@@ -1,21 +1,6 @@
-require 'irb/ruby-lex'
-require 'stringio'
-
 module Temple
   module Utils
     extend self
-
-    LITERAL_TOKENS = [RubyToken::TkSTRING, RubyToken::TkDSTRING]
-
-    def literal_string?(str)
-      lexer = RubyLex.new
-      lexer.set_input(StringIO.new(str.strip))
-
-      # The first token has to be a string.
-      LITERAL_TOKENS.include?(lexer.token.class) and
-      # That has to be the only token.
-      lexer.token.nil?
-    end
 
     def empty_exp?(exp)
       case exp[0]
