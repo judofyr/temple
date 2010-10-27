d3b999d01e0871660836ddbbe20f987bd3d65050
minad <mail@daniel-mendler.de>
2010-10-27 23:56:43 +0200

    filter for debugging purposes added

---
+++ lib/temple/filters/debugger.rb
@@ -0,0 +1,15 @@
+module Temple
+  module Filters
+    # Filter which prints Temple expression
+    class Debugger < BasicFilter
+      def compile(exp)
+        if @options[:debug]
+          puts @options[:prefix] if @options[:prefix]
+          puts exp.inspect
+          puts
+        end
+        exp
+      end
+    end
+  end
+end
