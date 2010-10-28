5ec04b18906aa2493f65e9762b3cda8ff449d704
minad <mail@daniel-mendler.de>
2010-10-28 15:49:03 +0200

    cleanup html filter a bit

--- lib/temple/html/fast.rb
+++ lib/temple/html/fast.rb
@@ -3,6 +3,22 @@ module Temple
     class Fast < Filters::BasicFilter
       temple_dispatch :html
 
+      XHTML_DOCTYPES = {
+        '1.1'          => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">',
+        '5'            => '<!DOCTYPE html>',
+        'strict'       => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
+        'frameset'     => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">',
+        'mobile'       => '<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">',
+        'basic'        => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN" "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">',
+        'transitional' => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
+      }.freeze
+
+      HTML4_DOCTYPES = {
+        'strict'       => '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">',
+        'frameset'     => '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">',
+        'transitional' => '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">',
+      }.freeze
+
       default_options.merge!({
         :format => :xhtml,
         :attr_wrapper => "'",
@@ -36,32 +52,18 @@ module Temple
         trailing_newlines = type[/(\A|[^\r])(\n+)\Z/, 2].to_s
 
         text = type.to_s.downcase.strip
-        if text.index("xml") == 0
-          if html?
-            return [:multi].concat([[:newline]] * trailing_newlines.size)
-          end
-
+        if text =~ /^xml/
+          raise 'Invalid xml directive in html mode' if html?
           wrapper = @options[:attr_wrapper]
           str = "<?xml version=#{wrapper}1.0#{wrapper} encoding=#{wrapper}#{text.split(' ')[1] || "utf-8"}#{wrapper} ?>"
-        end
-
-        str = "<!DOCTYPE html>" if html5?
-
-        str ||= if xhtml?
-          case text
-          when /^1\.1/;     '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">'
-          when /^5/;        '<!DOCTYPE html>'
-          when "strict";    '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
-          when "frameset";  '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">'
-          when "mobile";    '<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">'
-          when "basic";     '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN" "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">'
-          else              '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
-          end
-        elsif html4?
-          case text
-            when "strict";    '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">'
-            when "frameset";  '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">'
-            else              '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">'
+        else
+          case @options[:format]
+          when :html5
+            str = '<!DOCTYPE html>'
+          when :html4
+            str = HTML4_DOCTYPES[text] || HTML4_DOCTYPES['transitional']
+          when :xhtml
+            str = XHTML_DOCTYPES[text] || XHTML_DOCTYPES['transitional']
           end
         end
 
@@ -78,13 +80,10 @@ module Temple
 
       def on_html_tag(name, attrs, content, closed)
         closed ||= @options[:autoclose].include?(name)
-        raise "Autoclosed tag #{name} has content" if closed && !empty_exp?(content)
-        result = [:multi]
-        result << [:static, "<#{name}"]
-        result << compile(attrs)
+        raise "Closed tag #{name} has content" if closed && !empty_exp?(content)
+        result = [:multi, [:static, "<#{name}"], compile(attrs)]
         result << [:static, " /"] if closed && xhtml?
-        result << [:static, ">"]
-        result << compile(content)
+        result << [:static, ">"] << compile(content)
         result << [:static, "</#{name}>"] if !closed
         result
       end
@@ -121,9 +120,9 @@ module Temple
 
       def on_html_attr(name, value)
         [:multi,
-          [:static, " "],
+          [:static, ' '],
           [:static, name],
-          [:static, "="],
+          [:static, '='],
           [:static, @options[:attr_wrapper]],
           value,
           [:static, @options[:attr_wrapper]]]
