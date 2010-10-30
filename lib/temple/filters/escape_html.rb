7c52aaf539d05a7fc6ed44cbdb2181769d60e1cd
minad <mail@daniel-mendler.de>
2010-10-30 04:18:32 +0200

    fix escape html filter

--- lib/temple/filters/escape_html.rb
+++ lib/temple/filters/escape_html.rb
@@ -4,7 +4,7 @@ module Temple
       def on_escape(type, value)
         case type
         when :static
-          value = escape_html_safe(value)
+          value = @options[:user_html_safe] ? escape_html_safe(value) : escape_html(value)
         when :dynamic
           value = "Temple::Utils.escape_html#{@options[:use_html_safe] ? '_safe' : ''}((#{value}))"
         end
