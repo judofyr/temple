c3b881ba22b6a0e11caf5a11b35cf4c965e32069
minad <mail@daniel-mendler.de>
2010-10-31 18:49:55 +0100

    helper support removed

--- lib/temple/template.rb
+++ lib/temple/template.rb
@@ -13,14 +13,6 @@ module Temple
           raise 'No engine configured'
         end
       end
-
-      def helpers(helpers = nil)
-        if helpers
-          @helpers = helpers
-        else
-          @helpers
-        end
-      end
     end
 
     # Prepare Temple template
@@ -30,21 +22,6 @@ module Temple
     # @return [void]
     def prepare
       @src = self.class.engine.new(options.merge(:file => eval_file)).compile(data)
-    end
-
-    # Process the template and return the result.
-    #
-    # Template executationis guaranteed to be performed in the scope object with the locals
-    # specified and with support for yielding to the block.
-    #
-    # @param [Object] scope Scope object where the code is evaluated
-    # @param [Hash]   locals Local variables
-    # @yield Block given to the template code
-    # @return [String] Evaluated template
-    def evaluate(scope, locals, &block)
-      helpers = self.class.helpers
-      scope.instance_eval { extend helpers } if options[:helpers] && helpers
-      super
     end
 
     # A string containing the (Ruby) source code for the template.
