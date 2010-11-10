2f77af964ab5e8d9fee56a67e0acee9b61b8d9d5
minad <mail@daniel-mendler.de>
2010-11-10 20:08:20 +0100

    add test for preformatted tags

--- test/html/test_pretty.rb
+++ test/html/test_pretty.rb
@@ -20,4 +20,21 @@ describe Temple::HTML::Pretty do
                       [:static, "</p>"]],
                      [:static, "\n</div>"]]
   end
+
+
+  it 'should not indent preformatted tags' do
+    @html.compile([:html, :tag, 'pre', [:multi], false,
+      [:html, :tag, 'p', [:multi], false, [:static, 'text']]
+    ]).should.equal [:multi,
+                     [:static, "<pre"],
+                     [:multi],
+                     [:static, ">"],
+                     [:multi,
+                      [:static, "<p"],
+                      [:multi],
+                      [:static, ">"],
+                      [:static, "text"],
+                      [:static, "</p>"]],
+                     [:static, "\n</pre>"]]
+  end
 end
