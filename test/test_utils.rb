59fb246fe55610d837dd3646ffd8a6277dbba9d9
minad <mail@daniel-mendler.de>
2011-04-29 02:11:44 +0200

    add test for unique_name

--- test/test_utils.rb
+++ test/test_utils.rb
@@ -1,5 +1,9 @@
 require 'helper'
 
+class UniqueTest
+  include Temple::Utils
+end
+
 describe Temple::Utils do
   it 'has empty_exp?' do
     Temple::Utils.empty_exp?([:multi]).should.be.true
@@ -8,6 +12,13 @@ describe Temple::Utils do
     Temple::Utils.empty_exp?([:multi]).should.be.true
     Temple::Utils.empty_exp?([:multi, [:multi, [:static, 'text']]]).should.be.false
     Temple::Utils.empty_exp?([:multi, [:newline], [:multi, [:dynamic, 'text']]]).should.be.false
+  end
+
+  it 'has unique_name' do
+    u = UniqueTest.new
+    u.unique_name.should.equal '_uniquetest1'
+    u.unique_name.should.equal '_uniquetest2'
+    UniqueTest.new.unique_name.should.equal '_uniquetest1'
   end
 
   it 'has escape_html' do
