03ac3f76152f9eff8ff83b8146783001cf89441a
minad <mail@daniel-mendler.de>
2011-12-04 20:15:59 +0100

    erb parser: add missing [:newline] to result

--- lib/temple/erb/parser.rb
+++ lib/temple/erb/parser.rb
@@ -6,27 +6,28 @@ module Temple
     class Parser
       include Mixins::Options
 
-      ERB_PATTERN = /(<%%|%%>)|<%(==?|\#)?(.*?)?-?%>/m
-
-      ESCAPED = {
-        '<%%' => '<%',
-        '%%>' => '%>',
-      }.freeze
+      ERB_PATTERN = /(\n|<%%|%%>)|<%(==?|\#)?(.*?)?-?%>/m
 
       def call(input)
         result = [:multi]
         pos = 0
-        input.scan(ERB_PATTERN) do |escaped, indicator, code|
-          m = Regexp.last_match
-          text = input[pos...m.begin(0)]
-          pos  = m.end(0)
-          result << [:static, text] if !text.empty?
-          if escaped
-            result << [:static, ESCAPED[escaped]]
+        input.scan(ERB_PATTERN) do |token, indicator, code|
+          text = input[pos...$~.begin(0)]
+          pos  = $~.end(0)
+          if token
+            case token
+            when "\n"
+              result << [:static, "#{text}\n"] << [:newline]
+            when '<%%', '%%>'
+              result << [:static, text] unless text.empty?
+              token.slice!(1)
+              result << [:static, token]
+            end
           else
+            result << [:static, text] unless text.empty?
             case indicator
             when '#'
-              code.count("\n").times { result << [:newline] }
+              result << [:code, "\n" * code.count("\n")]
             when /=/
               result << [:escape, indicator.size <= 1, [:dynamic, code]]
             else
--- lib/temple/erb/trimming.rb
+++ lib/temple/erb/trimming.rb
@@ -10,18 +10,17 @@ module Temple
       def on_multi(*exps)
         case options[:trim_mode]
         when '>'
-          exps.each_cons(2) do |a, b|
-            if code?(a) && static?(b)
-              b[1].gsub!(/^\n/, '')
-            end
+          i = 0
+          while i < exps.size
+            exps.delete_at(i + 1) if code?(exps[i]) && exps[i+1] == [:static, "\n"]
+            i += 1
           end
         when '<>'
-          exps.each_with_index do |exp, i|
-            if code?(exp) &&
-                (!exps[i-1] || static?(exps[i-1]) && exps[i-1][1] =~ /\n$/) &&
-                (exps[i+1] && static?(exps[i+1]) && exps[i+1][1] =~ /^\n/)
-              exps[i+1][1].gsub!(/^\n/, '') if exps[i+1]
-            end
+          i = 0
+          while i < exps.size
+            exps.delete_at(i + 1) if code?(exps[i]) && exps[i+1] == [:static, "\n"] &&
+                                     (!exps[i-1] || (exps[i-1] == [:newline]))
+            i += 1
           end
         end
         [:multi, *exps]
@@ -31,10 +30,6 @@ module Temple
 
       def code?(exp)
         exp[0] == :escape || exp[0] == :code
-      end
-
-      def static?(exp)
-        exp[0] == :static
       end
     end
   end
