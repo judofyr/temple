add109c73b3d55446accf03e2a724b66b8d62589
HannesG <hag@informatik.uni-kiel.de>
2011-12-17 14:10:41 +0100

    Dispatcher allows to use arbitrary levels.

--- lib/temple/mixins/dispatcher.rb
+++ lib/temple/mixins/dispatcher.rb
@@ -51,40 +51,19 @@ module Temple
 
       private
 
-      def case_statement(types, level)
-        code = "case exp[#{level}]\n"
-        types.each do |name, method|
-          code << "when #{name.to_sym.inspect}\n" <<
-            (Hash === method ? case_statement(method, level + 1) : "#{method}(*(exp[#{level+1}..-1]))\n")
-        end
-        code << "else\nexp\nend\n"
-      end
-
       def dispatcher(exp)
         replace_dispatcher(exp)
       end
 
       def replace_dispatcher(exp)
-        types = {}
-        self.class.instance_methods.each do |method|
-          next if method.to_s !~ /^on_(.*)$/
-          method_types = $1.split('_')
-          (0...method_types.size).inject(types) do |tmp, i|
-            unless Hash === tmp
-              conflict = method_types[0...i].join('_')
-              raise "Temple dispatcher '#{method}' conflicts with 'on_#{conflict}'"
-            end
-            if i == method_types.size - 1
-              tmp[method_types[i]] = method
-            else
-              tmp[method_types[i]] ||= {}
-            end
-          end
+        tree = DispatchNode.new
+        dispatched_methods.each do |method|
+          method.split('_')[1..-1].inject(tree) {|node, type| node[type.to_sym] }.method = method
         end
         self.class.class_eval %{
           def dispatcher(exp)
             if self.class == #{self.class}
-              #{case_statement(types, 0)}
+              #{tree.compile}
             else
               replace_dispatcher(exp)
             end
@@ -92,9 +71,100 @@ module Temple
         }
         dispatcher(exp)
       end
+
+      def dispatched_methods
+        re = /^on(_[a-zA-Z0-9]+)*$/
+        self.methods.map(&:to_s).select(&re.method(:=~))
+      end
+
+      # @api private
+      class DispatchNode < Hash
+        attr_accessor :method
+
+        def initialize
+          super { |hsh,key| hsh[key] = DispatchNode.new }
+          @method = nil
+        end
+
+        def compile(level = 0, parent = nil)
+          if empty?
+            if method
+              ('  ' * level) + "#{method}(*exp[#{level}..-1])"
+            elsif !parent
+              'exp'
+            else
+              raise 'Invalid dispatcher node'
+            end
+          else
+            code = [('  ' * level) + "case(exp[#{level}])"]
+            each do |key, child|
+              code << ('  ' * level) + "when #{key.inspect}"
+              code << child.compile(level + 1, parent || method)
+            end
+            if method || !parent
+              code << ('  ' * level) + "else"
+              if method
+                code << ('  ' * level) + "  #{method}(*exp[#{level}..-1])"
+              else
+                code << ('  ' * level) + "  exp"
+              end
+            end
+            code << ('  ' * level) + "end"
+            code.join("\n")
+          end
+        end
+      end
     end
 
-    # @api private
+    # @api public
+    #
+    # Implements a compatible call-method
+    # based on the including classe's methods.
+    #
+    # It uses every method starting with
+    # "on" and uses the rest of the method
+    # name as prefix of the expression it
+    # will receive. So, if a dispatcher
+    # has a method named "on_x", this method
+    # will be called with arg0,..,argN
+    # whenever an expression like [:x, arg0,..,argN ]
+    # is encountered.
+    #
+    # This works with longer prefixes, too.
+    # For example a method named "on_y_z"
+    # will be called whenever an expression
+    # like [:y, :z, .. ] is found. Furthermore,
+    # if additionally a method named "on_y"
+    # is present, it will be called when an
+    # expression starts with :y but then does
+    # not contain with :z. This way a
+    # dispatcher can implement namespaces.
+    #
+    # @note
+    #  Processing does not reach into unknown
+    #  expression types by default.
+    #
+    # @example
+    #   class MyAwesomeDispatch
+    #     include Temple::Mixins::Dispatcher
+    #     def on_awesome(thing) # keep awesome things
+    #       return [:awesome, thing]
+    #     end
+    #     def on_boring(thing) # make boring things awesome
+    #       return [:awesome, thing+" with bacon"]
+    #     end
+    #     def on(type,*args) # unknown stuff is boring too
+    #       return [:awesome, 'just bacon']
+    #     end
+    #   end
+    #   filter = MyAwesomeDispatch.new
+    #   # Boring things are converted:
+    #   filter.call([:boring, 'egg']) #=> [:awesome, 'egg with bacon']
+    #   # Unknown things too:
+    #   filter.call([:foo]) #=> [:awesome, 'just bacon']
+    #   # Known but not boring things won't be touched:
+    #   filter.call([:awesome, 'chuck norris']) #=>[:awesome, 'chuck norris']
+    #
     module Dispatcher
       include CompiledDispatcher
       include CoreDispatcher
--- test/mixins/test_dispatcher.rb
+++ test/mixins/test_dispatcher.rb
@@ -7,8 +7,22 @@ class FilterWithDispatcherMixin
     [:on_test, arg]
   end
 
+  def on_test_check(arg)
+    [:on_check, arg]
+  end
+
   def on_second_test(arg)
     [:on_second_test, arg]
+  end
+
+  def on_seventh_level_level_level_level_level_test(arg)
+    [:on_seventh_test, arg]
+  end
+end
+
+class FilterWithDispatcherMixinAndOn < FilterWithDispatcherMixin
+  def on(*args)
+    [:on_zero, *args]
   end
 end
 
@@ -27,5 +41,17 @@ describe Temple::Mixins::Dispatcher do
 
   it 'should dispatch second level' do
     @filter.call([:second, :test, 42]).should.equal [:on_second_test, 42]
+  end
+
+  it 'should dispatch second level if prefixed' do
+    @filter.call([:test, :check, 42]).should.equal [:on_check, 42]
+  end
+
+  it 'should dispatch seventh level' do
+    @filter.call([:seventh, :level, :level, :level, :level, :level, :test, 42]).should == [:on_seventh_test, 42]
+  end
+
+  it 'should dispatch zero level' do
+    FilterWithDispatcherMixinAndOn.new.call([:foo,42]).should == [:on_zero, :foo, 42]
   end
 end
