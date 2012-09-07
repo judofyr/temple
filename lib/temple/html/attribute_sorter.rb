b617985d9ace0745a6b2baa825a66476c3aa86bb
Daniel Mendler <mail@daniel-mendler.de>
2012-09-07 16:27:50 +0200

    use stable attribute sorting

--- lib/temple/html/attribute_sorter.rb
+++ lib/temple/html/attribute_sorter.rb
@@ -10,9 +10,10 @@ module Temple
       end
 
       def on_html_attrs(*attrs)
+        n = 0 # Use n to make sort stable. This is important because the merger could be executed afterwards.
         [:html, :attrs, *attrs.sort_by do |attr|
           raise(InvalidExpression, 'Attribute is not a html attr') if attr[0] != :html || attr[1] != :attr
-          attr[2].to_s
+          [attr[2].to_s, n += 1]
         end]
       end
     end
