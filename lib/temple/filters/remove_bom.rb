6695fbeb5c1d5809aae95bfc9180f0bd539fb64c
Daniel Mendler <mail@daniel-mendler.de>
2014-12-03 09:08:59 +0100

    remove legacy code

--- lib/temple/filters/remove_bom.rb
+++ lib/temple/filters/remove_bom.rb
@@ -5,14 +5,10 @@ module Temple
     # @api public
     class RemoveBOM < Parser
       def call(s)
-        if s.respond_to?(:encoding)
-          if s.encoding.name =~ /^UTF-(8|16|32)(BE|LE)?/
-            s.gsub(Regexp.new("\\A\uFEFF".encode(s.encoding.name)), '')
-          else
-            s
-          end
+        if s.encoding.name =~ /^UTF-(8|16|32)(BE|LE)?/
+          s.gsub(Regexp.new("\\A\uFEFF".encode(s.encoding.name)), '')
         else
-          s.gsub(/\A\xEF\xBB\xBF/, '')
+          s
         end
       end
     end
