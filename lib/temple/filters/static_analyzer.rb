f3a6401eea8c16a593fc0e3e940f48f107a2a1a4
Takashi Kokubun <takashikkbn@gmail.com>
2015-12-24 01:22:11 +0900

    StaticAnalyzer should work even if ripper is unavailable

--- lib/temple/filters/static_analyzer.rb
+++ lib/temple/filters/static_analyzer.rb
@@ -1,36 +1,36 @@
-begin
-  require 'ripper'
-rescue LoadError
-end
+module Temple
+  module Filters
+    # Convert [:dynamic, code] to [:static, text] if code is static Ruby expression.
+    class StaticAnalyzer < Filter
+      STATIC_TOKENS = [
+        :on_tstring_beg, :on_tstring_end, :on_tstring_content,
+        :on_embexpr_beg, :on_embexpr_end,
+        :on_lbracket, :on_rbracket,
+        :on_qwords_beg, :on_words_sep, :on_qwords_sep,
+        :on_lparen, :on_rparen,
+        :on_lbrace, :on_rbrace, :on_label,
+        :on_int, :on_float, :on_imaginary,
+        :on_comma, :on_sp,
+      ].freeze
 
-if defined?(Ripper)
-  module Temple
-    module Filters
-      # Convert [:dynamic, code] to [:static, text] if code is static Ruby expression.
-      class StaticAnalyzer < Filter
-        STATIC_TOKENS = [
-          :on_tstring_beg, :on_tstring_end, :on_tstring_content,
-          :on_embexpr_beg, :on_embexpr_end,
-          :on_lbracket, :on_rbracket,
-          :on_qwords_beg, :on_words_sep, :on_qwords_sep,
-          :on_lparen, :on_rparen,
-          :on_lbrace, :on_rbrace, :on_label,
-          :on_int, :on_float, :on_imaginary,
-          :on_comma, :on_sp,
-        ].freeze
+      DYNAMIC_TOKENS = [
+        :on_ident, :on_period,
+      ].freeze
 
-        DYNAMIC_TOKENS = [
-          :on_ident, :on_period,
-        ].freeze
+      STATIC_KEYWORDS = [
+        'true', 'false', 'nil',
+      ].freeze
 
-        STATIC_KEYWORDS = [
-          'true', 'false', 'nil',
-        ].freeze
+      STATIC_OPERATORS = [
+        '=>',
+      ].freeze
 
-        STATIC_OPERATORS = [
-          '=>',
-        ].freeze
+      begin
+        require 'ripper'
+      rescue LoadError
+      end
 
+      if defined?(Ripper)
         def self.static?(code)
           return false if code.nil? || code.strip.empty?
           return false if SyntaxChecker.syntax_error?(code)
@@ -75,6 +75,11 @@ if defined?(Ripper)
           def on_parse_error(*)
             raise ParseError
           end
+        end
+      else
+        # Do nothing if ripper is unavailable
+        def call(ast)
+          ast
         end
       end
     end
